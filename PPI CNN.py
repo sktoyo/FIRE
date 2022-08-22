fix wrong normalization

PPI CNN.py
@@ -1,17 +1,19 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Dropout, Input, Reshape, BatchNormalization
+from tensorflow.keras.layers import Dense, Dropout, Input, Reshape, BatchNormalization, Activation, MaxPool1D
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, MaxPool1D, Conv1D
 from tensorflow.keras.models import Model
 from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import to_categorical, plot_model
 
 # input feature shape: [(None, (2, 512, 20)], 10000 positive pairs, 10000 negative pairs, 2000 test pairs(pos/neg)
 def concat_dataset(data1, data2, axis):
     return np.concatenate((data1, data2), axis=axis)
 
 def create_dataset(size):
-
     protein_1 = np.random.rand(size, 1, 512, 20) + 10
     protein_2 = np.random.rand(size, 1, 512, 20) + 20
 
@@ -31,31 +33,48 @@ def create_dataset(size):
     return x, y
 
 
-# x_train, y_train = create_dataset(10000)
-# x_test, y_test = create_dataset(1000)
-#
-# x_train = (x_train.astype('float32') - x_train.min()) / (x_train.max() - x_train.min())
-# x_test = (x_test.astype('float32') - x_train.min()) / (x_train.max() - x_train.min())
+def create_dataset_constant(size):
+    protein_1 = np.zeros((size, 1, 512, 20)) + 10
+    protein_2 = np.zeros((size, 1, 512, 20)) + 20
 
-# from sklearn.manifold import TSNE
-# tsne = TSNE(n_components=2, random_state=0)
-# x_train_vis = np.concatenate((x_train[:50], x_train[-50:]), axis=0)
-# x_train_vis = x_train_vis.reshape((-1, 2*512*20))
-# x_test_vis = np.concatenate((x_test[:50], x_test[-50:]), axis=0)
-# x_test_vis = x_train_vis.reshape((-1, 2*512*20))
-# total_2d = tsne.fit_transform(concat_dataset(x_train_vis, x_test_vis,0))
-#
-# from matplotlib import pyplot as plt
-# plt.figure(figsize=(6, 5))
-# colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple'
-# plt.scatter(total_2d[:50][:,0], total_2d[:50][:,1], c='r', label='train_neg')
-# plt.scatter(total_2d[50:100][:,0], total_2d[50:100][:,1], c='g', label='train_pos')
-# plt.scatter(total_2d[100:150][:,0], total_2d[100:150][:,1], c='b', label='test_neg')
-# plt.scatter(total_2d[150:][:,0], total_2d[150:][:,1], c='y', label='test_pos')
-# plt.legend()
-# plt.show()
+    p1_p2 = concat_dataset(protein_1, protein_2, 1)
+    p2_p1 = concat_dataset(protein_2, protein_1, 1)
+    pos = concat_dataset(p1_p2, p2_p1, 0)
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    protein_3 = np.zeros((size, 1, 512, 20)) + 30
+    protein_4 = np.zeros((size, 1, 512, 20)) + 40
+
+    p3_p4 = concat_dataset(protein_3, protein_4, 1)
+    p4_p3 = concat_dataset(protein_4, protein_3, 1)
+    neg = concat_dataset(p3_p4, p4_p3, 0)
+
+    x = concat_dataset(pos, neg, 0)
+    y = concat_dataset(np.ones(size*2), np.zeros(size*2), 0)
+    return x, y
+
+x_train, y_train = create_dataset(1000)
+x_test, y_test = create_dataset(100)
+
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# visualize data distribution
+from sklearn.manifold import TSNE
+tsne = TSNE(n_components=2, random_state=0)
+x_train_vis = np.concatenate((x_train[:50], x_train[-50:]), axis=0)
+x_train_vis = x_train_vis.reshape((-1, 2*512*20))
+x_test_vis = np.concatenate((x_test[:50], x_test[-50:]), axis=0)
+x_test_vis = x_train_vis.reshape((-1, 2*512*20))
+total_2d = tsne.fit_transform(concat_dataset(x_train_vis, x_test_vis,0))
+
+from matplotlib import pyplot as plt
+plt.figure(figsize=(6, 5))
+colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple'
+plt.scatter(total_2d[:50][:,0], total_2d[:50][:,1], c='r', label='train_neg')
+plt.scatter(total_2d[50:100][:,0], total_2d[50:100][:,1], c='g', label='train_pos')
+plt.scatter(total_2d[100:150][:,0], total_2d[100:150][:,1], c='b', label='test_neg')
+plt.scatter(total_2d[150:][:,0], total_2d[150:][:,1], c='y', label='test_pos')
+plt.legend()
+plt.show()
 
 # from sparse label to categorical
 num_labels = len(np.unique(y_train))
@@ -63,22 +82,24 @@ y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
 # resize and normalize
-# x_train = np.reshape(x_train, [-1, 2, 512, 20])
-# x_test = np.reshape(x_test, [-1, 2, 512, 20])
-x_train = np.reshape(x_train, [-1, 28, 28, 1]) # MNIST test
-x_test = np.reshape(x_test, [-1, 28, 28, 1]) # MNIST test
-x_train = (x_train.astype('float32') - x_train.min()) / (x_train.max() - x_train.min())
-x_test = (x_test.astype('float32') - x_train.min()) / (x_train.max() - x_train.min())
+x_train = np.reshape(x_train, [-1, 2, 512, 20])
+x_test = np.reshape(x_test, [-1, 2, 512, 20])
+# x_train = np.reshape(x_train, [-1, 28, 28, 1]) # MNIST test
+# x_test = np.reshape(x_test, [-1, 28, 28, 1]) # MNIST test
+x_max = x_train.max()
+x_min = x_train.min()
+x_train = (x_train.astype('float32') - x_min) / (x_max - x_min)
+x_test = (x_test.astype('float32') - x_min) / (x_max - x_min)
 
 print(x_train.shape)
 print(x_test.shape)
 # network parametres
-# input_shape = (2, 512, 20)
-input_shape = (28, 28, 1) # MNIST test
-batch_size = 1024
+input_shape = (2, 512, 20)
+# input_shape = (28, 28, 1) # MNIST test
+batch_size = 128
 kernel_size = 3
-filters = 16
-dropout = 0.9
+filters = 64
+dropout = 0.3
 
 # network setting
 def conv2D_model():
@@ -106,13 +127,16 @@ def conv1D_model():
     y = Conv2D(filters,
                kernel_size=(2, kernel_size),
                strides=2,
-               activation='relu')(inputs)
-    y = tf.squeeze(y, [1])
+               )(inputs)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
+    y = Reshape((int(input_shape[0]/2*(input_shape[1]/2-1)), filters))(y)
     y = MaxPool1D()(y)
     y = Conv1D(filters*2,
                kernel_size=kernel_size,
-               strides=2,
-               activation='relu')(y)
+               strides=2)(y)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
     # y = MaxPool1D()(y)
     # y = Conv1D(filters,
     #            kernel_size=kernel_size,
@@ -126,9 +150,10 @@ def conv1D_model():
     return Model(inputs=inputs, outputs=outputs)
 
 # build the model by spplying inputs/outputs
-model = conv2D_model()
+model = conv1D_model()
 # network model in text
 model.summary()
+plot_model(model, to_file='conv1d_toy_data.png', show_shapes=True)
 
 # classifier loss, Adam optimizer, classifier accuracy
 model.compile(loss='categorical_crossentropy',
@@ -138,7 +163,7 @@ model.compile(loss='categorical_crossentropy',
 model.fit(x_train,
           y_train,
           validation_data=(x_test, y_test),
-          epochs=20,
+          epochs=5,
           batch_size=batch_size)
 
 # model accuracy on test dataset
@@ -148,6 +173,7 @@ score = model.evaluate(x_test,
                        verbose=0)
 print('\nTest accuracyL %.1f%%' % (100.0 * score[1]))
 
-print(model(x_test))
+print(model(x_test[:10]))
+print(model(x_test[-10:]))
 print(model(x_train[:10]))
 print(model(x_train[-10:]))

simpleCNN.py
@@ -1,3 +1,6 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
 import numpy as np
 from tensorflow.keras.layers import Dense, Dropout, Input
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten
@@ -5,25 +8,36 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 
-def create_dataset(size):
-
-    protein_1 = np.random.rand(size, 28, 28) + 10
-
-    protein_2 = np.random.rand(size, 28, 28) + 30
-
-    x = concat_dataset(protein_1, protein_2, 0)
-    y = concat_dataset(np.ones(size), np.zeros(size), 0)
-    return x, y
-
 def concat_dataset(data1, data2, axis):
     return np.concatenate((data1, data2), axis=axis)
 
+def create_dataset(size):
+
+    protein_1 = np.random.rand(size, 28, 28, 1) + 10
+    protein_2 = np.random.rand(size, 28, 28, 1) + 20
+
+    p1_p2 = concat_dataset(protein_1, protein_2, 3)
+    p2_p1 = concat_dataset(protein_2, protein_1, 3)
+    pos = concat_dataset(p1_p2, p2_p1, 0)
+
+    protein_3 = np.random.rand(size, 28, 28, 1) + 30
+    protein_4 = np.random.rand(size, 28, 28, 1) + 40
+
+    p3_p4 = concat_dataset(protein_3, protein_4, 3)
+    p4_p3 = concat_dataset(protein_4, protein_3, 3)
+    neg = concat_dataset(p3_p4, p4_p3, 0)
+
+    x = concat_dataset(pos, neg, 0)
+    y = concat_dataset(np.ones(size*2), np.zeros(size*2), 0)
+    return x, y
+
+
 # load MNIST dataset
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 # Random test data
-x_train, y_train = create_dataset(30000)
-x_test, y_test = create_dataset(3000)
+# x_train, y_train = create_dataset(30000)
+# x_test, y_test = create_dataset(3000)
 
 # from sparse label to categorical
 num_labels = len(np.unique(y_train))
@@ -72,7 +86,7 @@ model.compile(loss='categorical_crossentropy',
 model.fit(x_train,
           y_train,
           validation_data=(x_test, y_test),
-          epochs=20,
+          epochs=5,
           batch_size=batch_size)
 
 # model accuracy on test dataset